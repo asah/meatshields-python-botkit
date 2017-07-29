06db998972e679a18786f1d749dd88d0de8aa17e
Adam Sah <asah@midgard.net>
2017-07-29 17:45:59 +0000

    test_nextmove.sh: optional first param to specify the .json board file

--- test_nextmove.sh
+++ test_nextmove.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" -X POST --data-ascii @example_getNextMove.json http://localhost:5000/meatshields/bot/getNextMove
+JSON_FILE="$1"
+if [ "x$JSON_FILE" = "x" ]; then
+    JSON_FILE="example_getNextMove.json"
+fi
+
+curl -H "Content-Type: application/json" -X POST --data-ascii @$JSON_FILE http://localhost:5000/meatshields/bot/getNextMove
