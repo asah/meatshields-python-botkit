3c137725b22a5205c2fe414be35269222a285714
Adam Sah <asah@midgard.net>
2017-07-29 04:11:57 +0000

    run.sh: support ./run.sh [bot.py] to specify the bot

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 export FLASK_DEBUG=1
-export FLASK_APP=nullbot.py
+export FLASK_APP="$1"
+if [ "x$FLASK_APP" = "x" ]; then
+    export FLASK_APP=nullbot.py
+fi
 python3 -m flask run --host=0.0.0.0
